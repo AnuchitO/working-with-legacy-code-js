bd6127ee1ba64fb530d30c73deb96b451ce712c5
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-01 20:17:34 +0700

    move amount for inside

--- src/statement.js
+++ src/statement.js
@@ -4,6 +4,28 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID]
     }
 
+    function amountFor(aPerformance, play) {
+        let result = 0
+        switch (play.type) {
+            case "tragedy":
+                result = 40000
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break
+            case "comedy":
+                result = 30000
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience
+                break
+            default:
+                throw new Error(`unknown type: ${play.type}`)
+        }
+        return result
+    }
+
     let totalAmount = 0
     let volumeCredits = 0
     let result = `Statement for ${invoice.customer}\n`
@@ -30,27 +52,6 @@ function statement(invoice, plays) {
     return result
 }
 
-function amountFor(aPerformance, play) {
-    let result = 0
-    switch (play.type) {
-        case "tragedy":
-            result = 40000
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30)
-            }
-            break
-        case "comedy":
-            result = 30000
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20)
-            }
-            result += 300 * aPerformance.audience
-            break
-        default:
-            throw new Error(`unknown type: ${play.type}`)
-    }
-    return result
-}
 
 module.exports = {
     statement,
