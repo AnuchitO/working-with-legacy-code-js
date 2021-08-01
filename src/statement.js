052e2319e99e02ed96f020845d01e51de1f3aa6e
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-01 20:19:06 +0700

    #7 Remove Temporary Query : amount for

--- src/statement.js
+++ src/statement.js
@@ -4,7 +4,7 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID]
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0
         switch (playFor(aPerformance).type) {
             case "tragedy":
@@ -36,7 +36,7 @@ function statement(invoice, plays) {
         }).format
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf))
+        let thisAmount = amountFor(perf)
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0)
