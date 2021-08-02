20f57466569e17f913ff4103adf84130d9b17121
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 15:37:56 +0700

    #13 move part of formatting to usd function

--- src/statement.js
+++ src/statement.js
@@ -41,7 +41,7 @@ function statement(invoice, plays) {
             {
                 style: "currency", currency: "USD",
                 minimumFractionDigits: 2
-            }).format(aNumber);
+            }).format(aNumber / 100);
     }
 
     let totalAmount = 0
@@ -54,10 +54,10 @@ function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf)
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
         totalAmount += amountFor(perf)
     }
-    result += `Amount owed is ${usd(totalAmount / 100)}\n`
+    result += `Amount owed is ${usd(totalAmount)}\n`
     result += `You earned ${volumeCredits} credits\n`
     return result
 }
