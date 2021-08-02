72df8c7acae701cfae37c843bb7b279f92b29e97
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 15:48:18 +0700

    #18 extract function : name it plara becuase the name it crash with variable name totalAmount

--- src/statement.js
+++ src/statement.js
@@ -53,13 +53,23 @@ function statement(invoice, plays) {
         return volumeCredits
     }
 
-    let totalAmount = 0
+    function plara() {
+        let totalAmount = 0
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf)
+        }
+
+        return totalAmount
+    }
+
     let result = `Statement for ${invoice.customer}\n`
     for (let perf of invoice.performances) {
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
-        totalAmount += amountFor(perf)
     }
+
+    let totalAmount = plara()
+
     result += `Amount owed is ${usd(totalAmount)}\n`
     result += `You earned ${totalVolumeCredits()} credits\n`
     return result
