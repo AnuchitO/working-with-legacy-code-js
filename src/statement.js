b277448751efc19aad87e90d2b222fa1ba8b0551
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 20:06:10 +0700

    #28 copy amoutFor to statement

--- src/statement.js
+++ src/statement.js
@@ -9,11 +9,34 @@ function statement(invoice, plays) {
     function enrichPerforrmance(aPerformance) {
         const result = Object.assign({}, aPerformance)
         result.play = playFor(result)
+        result.amount = amountFor(result)
         return result
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID]
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0
+        switch (aPerformance.play.type) {
+            case "tragedy":
+                result = 40000
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break
+            case "comedy":
+                result = 30000
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience
+                break
+            default:
+                throw new Error(`unknown type: ${aPerformance.play.type}`)
+        }
+        return result
     }
 }
 
