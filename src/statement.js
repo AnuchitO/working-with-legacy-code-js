796f0948708600373b189b68c933d58a83467e68
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 15:30:50 +0700

    #10 rename - make it more clear and have same pattern

--- src/statement.js
+++ src/statement.js
@@ -26,14 +26,14 @@ function statement(invoice, plays) {
         return result
     }
 
-    function volumeCreditsFor(perf) {
-        let volumeCredits = 0
+    function volumeCreditsFor(aPerformance) {
+        let result = 0
         // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0)
+        result += Math.max(aPerformance.audience - 30, 0)
         // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
 
-        return volumeCredits
+        return result
     }
 
     let totalAmount = 0
