6c06197b5c5d00a64ed403bc8e64df737cac72c9
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 20:07:54 +0700

    #29 remove old amountFor

--- src/statement.js
+++ src/statement.js
@@ -53,28 +53,6 @@ function renderPlainText(data) {
     result += `You earned ${totalVolumeCredits()} credits\n`
     return result
 
-    function amountFor(aPerformance) {
-        let result = 0
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-                break
-            case "comedy":
-                result = 30000
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20)
-                }
-                result += 300 * aPerformance.audience
-                break
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`)
-        }
-        return result
-    }
-
     function volumeCreditsFor(aPerformance) {
         let result = 0
         // add volume credits
