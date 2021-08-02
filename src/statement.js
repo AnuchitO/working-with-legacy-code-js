309dcb169dd6bdd093954eaebfd45fcdd2d71bef
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 20:04:51 +0700

    #27 remove play parameter

--- src/statement.js
+++ src/statement.js
@@ -3,7 +3,7 @@ function statement(invoice, plays) {
     const statementData = {}
     statementData.customer = invoice.customer
     statementData.performances = invoice.performances.map(enrichPerforrmance)
-    return renderPlainText(statementData, plays)
+    return renderPlainText(statementData)
 
 
     function enrichPerforrmance(aPerformance) {
@@ -19,7 +19,7 @@ function statement(invoice, plays) {
 
 
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`
     for (let perf of data.performances) {
         // print line for this order
