b7066dbbc686de5f91c786a0316ef4e07b232c9a
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 15:34:36 +0700

    #11 relace temporary function : format

--- src/statement.js
+++ src/statement.js
@@ -36,14 +36,18 @@ function statement(invoice, plays) {
         return result
     }
 
+    function format(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber);
+    }
+
     let totalAmount = 0
     let volumeCredits = 0
     let result = `Statement for ${invoice.customer}\n`
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format
+
 
     for (let perf of invoice.performances) {
         // add volume credits
