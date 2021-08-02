cbbf78a3bd21b12919973492192da2698de44dd5
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 15:36:14 +0700

    #12 rename function declaration : because it format as USD

--- src/statement.js
+++ src/statement.js
@@ -36,7 +36,7 @@ function statement(invoice, plays) {
         return result
     }
 
-    function format(aNumber) {
+    function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
             {
                 style: "currency", currency: "USD",
@@ -54,10 +54,10 @@ function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf)
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
         totalAmount += amountFor(perf)
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`
+    result += `Amount owed is ${usd(totalAmount / 100)}\n`
     result += `You earned ${volumeCredits} credits\n`
     return result
 }
