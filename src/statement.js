f041fc2006332112dae321612d11ebfcdbbea26e
AnuchitO <anuchit.prasertasng@gmail.com>
2021-08-02 20:07:29 +0700

    #29 use perf.amount

--- src/statement.js
+++ src/statement.js
@@ -46,7 +46,7 @@ function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`
     for (let perf of data.performances) {
         // print line for this order
-        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`
     }
 
     result += `Amount owed is ${usd(totalAmount())}\n`
@@ -105,7 +105,7 @@ function renderPlainText(data) {
     function totalAmount() {
         let result = 0
         for (let perf of data.performances) {
-            result += amountFor(perf)
+            result += perf.amount
         }
 
         return result
